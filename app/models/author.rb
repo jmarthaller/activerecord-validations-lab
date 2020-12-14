f5f4c220e12784af6c148a8ac0e7674534f8e2ae
Jacob Marthaller <https://github.com/jmarthaller>
2020-12-14 17:37:17 -0500

    validations secured

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+    validates_uniqueness_of :name, acceptance: true
+    validates :name, length: { minimum: 1 }
+    validates :phone_number, length: { is: 10 }
+    
 end
